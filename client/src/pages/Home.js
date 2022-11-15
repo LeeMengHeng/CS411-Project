61d65e3194c95c0bb1a3f3f739cb6885d95676ac
Marielle <mbelomme@bu.edu>
2022-11-15 18:10:59 -0500

    Added three functional components for webpage.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,37 +1,30 @@
 import React, {useState, useEffect} from "react"
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, useNavigate, createSearchParams, generatePath} from 'react-router-dom';
 import background from '../img/background.png'
 import Search from "./Search";
 
 function Home() {
-  // const state = {
-  //   input: ''
-  // }
   const [input, setName] = useState("")
   const navigate = useNavigate()
   const handleClick = (event) => {
-    // this.setState({input:{input}})
-    event.preventDefault()
-    navigate(`/search/${input}`, {input: input})
-    console.log({input})
-    // console.log('string: ', state.input)
-    alert(`https://images-api.nasa.gov/search?q=${input}`)
+    // event.preventDefault()
+    navigate(`/search/${input}`)
   }
 
     return (
-        <div>
-            <label style = {{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '53rem'}}>
-                <input
-                type = "text" 
-                value = {input}
-                onChange = {(e) => setName(e.target.value)}
-                style = {{borderRadius: '50px', border: 'solid', borderColor: 'white', height: '30px', width: '300px', textAlign: 'center', background: 'none', color: 'white'}}
-                placeholder = 'start exploring here...'
-                />
-                <button type = 'button' onClick = {handleClick} style = {{position: 'absolute', top: '470px', height: '40px', width: '100px', borderRadius: '50px', border: 'none', fontSize: '17px', fontFamily: 'revert', color: '#02004C'}}>Explore</button>
-            </label>
-        </div>
+      <div>
+        <label style = {{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '53rem'}}>
+          <input
+            type = "text" 
+            value = {input}
+            onChange = {(e) => setName(e.target.value)}
+            style = {{borderRadius: '50px', border: 'solid', borderColor: 'white', height: '30px', width: '300px', textAlign: 'center', background: 'none', color: 'white'}}
+            placeholder = 'start exploring here...'
+          />
+          <button type = 'button' onClick = {handleClick} style = {{position: 'absolute', top: '470px', height: '40px', width: '100px', borderRadius: '50px', border: 'none', fontSize: '17px', fontFamily: 'revert', color: '#02004C'}}>Explore</button>
+        </label>
+      </div>
     )
 }
 
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,69 +1,36 @@
 import React, {useState, useEffect} from "react"
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter, Routes, Route, useNavigate, useLocation, useSearchParams} from 'react-router-dom';
-import background from '../img/background.png'
-import Home from "./Home";
+import {useParams} from 'react-router-dom';
 
-import { Component } from "react";
+function Search(){
+  const params = useParams()
 
+  const [data, setData] = useState("")
+  const {input} = params
 
+  useEffect(() => {
+    fetch(`https://images-api.nasa.gov/search?q=${input}&media_type=image`)
+      .then(res => res.json())
+      .then(data => { 
+        setData(data)
+      })
+  },[])
 
-class Search extends Component {
-    constructor(props) {
-      super(props)
-      console.log(props)
-    }
+  if (data) {
+    return (
+      <div style={{display: "flex",justifyContent:"center"}}>
+        <div style={{color: "white", alignItems:'center'}}>
+          <h1>{data.collection.items[0].data[0].title}</h1>
+          <img src={data.collection.items[0].links[0].href}/>
+          <p>Description: {data.collection.items[0].data[0].description_508}</p>
+        </div>
+      </div>
+    )
+  } else {
+    return null
+  }
 
-    render() {
-      return (
-          <div id="div">
-            <h1>test</h1>
-          </div>
-      )
-    }
-}
-
-function Searchs({route}){
-  route = useSearchParams()
-  console.log(this.props.match.params)
-  // const [data, setData] = useState([])
-  // const {input} = route.params 
-  // console.log({data})
-  // console.log('input: ', {input})
-  // useEffect(() => {
-  //   fetch(`https://images-api.nasa.gov/search?q=${input}&media_type=image`)
-  //     .then(res => res.json())
-  //     .then(data => 
-  //        { 
-  //         setData(data)})
-  //     .catch(err => {
-  //       console.log(err)
-  //     })
-  // }, [])
-
-  // // fetch(`https://images-api.nasa.gov/search?q=${input}&media_type=image`)
-  // // .then(res => res.json())
-  // // .then(data => con{setData(data)})
-  // // .catch(err => {
-  // //   console.log(err)
-  // // })
-  // // useEffect(() => {
-
-  // // }, [])
-
-  // return (
-  //   <div>
-  //     <h1 style = {{color: 'white'}}>hi</h1>
-  //     <img src={data.collection.items[0].links[0].href}></img>
-  //   </div>
-  //   // <div className="App" style = {{position: 'absolute', top: '100px'}}>
-  //   //   <h1>{data.title}</h1>
-  //   //   <img src={data.hdurl} style = {{width: '800px', height: 'auto'}}></img>
-  //   //   <p>{data.explanation}</p>
-  //   // </div>
-  // )
-  // // https://api.nasa.gov/planetary/apod?api_key=iCjqUCjeREgC5J8gcdt13divgZ14uDzN13TTIAXh
-  // // https://images-api.nasa.gov/search?q=${input}
+  // https://api.nasa.gov/planetary/apod?api_key=iCjqUCjeREgC5J8gcdt13divgZ14uDzN13TTIAXh
+  // https://images-api.nasa.gov/search?q=${input}
 }
 
 export default Search
