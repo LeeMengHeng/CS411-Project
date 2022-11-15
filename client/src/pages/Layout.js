08c14f639b8ab591a1b8c25219a3e7262a9120c0
Marielle <mbelomme@bu.edu>
2022-11-15 18:10:59 -0500

    Added three functional components for webpage.

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -3,17 +3,6 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      {/* <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-        </ul>
-      </nav> */}
-
       <Outlet />
     </>
   )
