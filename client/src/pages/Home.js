4d112e4104ed4a53a5e2d5f76a33db14596a2356
LeeMengHeng <30385825+LeeMengHeng@users.noreply.github.com>
2022-11-21 13:20:27 -0500

    added background image

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react"
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter, Routes, Route, useNavigate, createSearchParams, generatePath} from 'react-router-dom';
-import background from '../img/background.png'
 import Search from "./Search";
 
 function Home() {
